cfa4911451259cbe772c52ba2228fe31181a342f
hoinx-0899 <nguyen.xuan.hoi@sun-asterisk.com>
2019-07-15 09:19:24 +0700

    create base fragment

--- app/src/main/java/com/sun/music_66/MainActivity.kt
+++ app/src/main/java/com/sun/music_66/MainActivity.kt
@@ -3,10 +3,13 @@ package com.sun.music_66
 import com.sun.music_66.base.view.BaseActivity
 
 class MainActivity : BaseActivity() {
+
     override fun getContentViewId(): Int = R.layout.activity_main
 
-    override fun initializeContainer() {}
+    override fun initializeContainer() {
+    }
 
-    override fun initializeComponents() {}
+    override fun initializeComponents() {
+    }
 
 }
